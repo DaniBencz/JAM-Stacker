237df7e93dd8fe05491489f638adafd204fb77e8
Daniel <daniel.bencz@gmail.com>
2020-06-08 12:59:06 +0200

    state

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { useState } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
@@ -6,20 +7,22 @@ import SEO from "../components/seo"
 import Display from "../components/display"
 
 const IndexPage = () => {
+  let [jam, setJam] = useState(0)
 
   const eatJam = () => {
-    alert("Eat")
-
+    setJam(prev => prev - 1)
+    console.log(jam)
   }
   const stackJam = () => {
-    alert("Stack")
+    setJam(prev => prev + 1)
+    console.log(jam)
   }
 
   return (
     <Layout>
       <SEO title="Home" />
       <h1>Chill out, it's ok!</h1>
-      <Display jam={2} eatJam={() => eatJam()} stackJam={() => stackJam()}></Display>
+      <Display jam={jam} eatJam={() => eatJam()} stackJam={() => stackJam()}></Display>
       <Link to="/redux-page/">Page with Redux</Link> <br />
     </Layout>
   )
