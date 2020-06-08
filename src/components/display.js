fe704c029e09b87bb760258ac4fd15a3657d35ef
Daniel <daniel.bencz@gmail.com>
2020-06-08 11:53:02 +0200

    Dipslay

---
+++ src/components/display.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Display = ({ jam, eatJam, stackJam }) => {
+
+  return (
+    <div>
+      <button onClick={() => { eatJam() }}>Eat Jam</button>
+      <p>{jam}</p>
+      <button onClick={() => { stackJam() }}>Stack Jam</button>
+    </div>
+  )
+}
+
+export default Display
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,27 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Display from "../components/display"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Chill out, it's ok!</h1>
-    <Link to="/redux-page/">Page with Redux</Link> <br />
-  </Layout>
-)
+const IndexPage = () => {
+
+  const eatJam = () => {
+    alert("Eat")
+
+  }
+  const stackJam = () => {
+    alert("Stack")
+  }
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Chill out, it's ok!</h1>
+      <Display jam={2} eatJam={() => eatJam()} stackJam={() => stackJam()}></Display>
+      <Link to="/redux-page/">Page with Redux</Link> <br />
+    </Layout>
+  )
+}
 
 export default IndexPage
